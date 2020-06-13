7af64bff5098a8fedf9dbe2c6af31b0be876c680
Olga Titova <olgtit@hot.ee>
2020-06-13 23:27:41 +0300

    L0024 collections, ArrayList, LinkedList

--- edu/javacourse/studentorder/validator/CityRegisterValidator.java
+++ edu/javacourse/studentorder/validator/CityRegisterValidator.java
@@ -1,11 +1,15 @@
 package edu.javacourse.studentorder.validator;
 
+import edu.javacourse.studentorder.domain.Child;
 import edu.javacourse.studentorder.domain.StudentOrder;
 import edu.javacourse.studentorder.domain.register.AnswerCityRegister;
 import edu.javacourse.studentorder.domain.register.CityRegisterCheckerResponce;
 import edu.javacourse.studentorder.exception.CityRegisterException;
 import edu.javacourse.studentorder.validator.register.CityRegisterChecker;
 import edu.javacourse.studentorder.validator.register.FakeCityRegisterChecker;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class CityRegisterValidator {
 
@@ -19,7 +23,19 @@ public class CityRegisterValidator {
         try {
             CityRegisterCheckerResponce hans = personChecker.checkPerson(so.getHusband());
             CityRegisterCheckerResponce wans = personChecker.checkPerson(so.getWife());
-            CityRegisterCheckerResponce cans = personChecker.checkPerson(so.getChild());
+            List<Child> children = so.getChildren();
+            for (int i = 0; i < children.size(); i++) {
+                CityRegisterCheckerResponce cans = personChecker.checkPerson(children.get(i));
+            }
+
+            for (Iterator<Child> it = children.iterator(); it.hasNext(); ) {
+                Child child = it.next();
+                CityRegisterCheckerResponce cans = personChecker.checkPerson(child);
+            }
+
+            for (Child child : children) {
+                CityRegisterCheckerResponce cans = personChecker.checkPerson(child);
+            }
         } catch (CityRegisterException ex) {
             ex.printStackTrace(System.out);
         }
